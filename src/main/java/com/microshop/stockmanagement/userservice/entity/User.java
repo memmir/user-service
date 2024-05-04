94df6e1f7884a4e3018aa13d5c5eef810348edce
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-04 16:09:47 +0300

    Created entity class and repository class.

---
+++ src/main/java/com/microshop/stockmanagement/userservice/entity/User.java
@@ -0,0 +1,4 @@
+package com.microshop.stockmanagement.userservice.entity;
+
+public class User {
+}
---
+++ src/main/java/com/microshop/stockmanagement/userservice/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.microshop.stockmanagement.userservice.repository;
+
+public interface UserRepository {
+}
