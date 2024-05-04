37b806af1e0c3c4dc7a803fd506b5de11ea1e07d
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-05 01:53:54 +0300

    Created user entity class.

--- src/main/java/com/microshop/stockmanagement/userservice/entity/User.java
+++ src/main/java/com/microshop/stockmanagement/userservice/entity/User.java
@@ -1,4 +1,56 @@
 package com.microshop.stockmanagement.userservice.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_surname")
+    private String userSurname;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @Column(name = "user_address")
+    private String userAddress;
+
+    @Builder.Default // bu alanı constructor da default olarak her zaman parametre olarak geçmesini sağlayan annottasyon.
+    @Column(name = "product_updated_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date productUpdatedDate = new Date();
+
+
+    @Builder.Default
+    @Column(name = "product_created_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date productCreatedDate = new Date();
+
+    @Column(name = "is_deleted") // Bu kolon veritabanında soft delete yapmamızı sağlıyor. True ya çektiğimizde silinmiş gibi olacak
+    private boolean deleted;
 }
