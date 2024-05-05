c8a3a70dae72a6fc1636bd05d5f8f9d30b5c56ab
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-05 17:55:30 +0300

    Created user service interface.

--- src/main/java/com/microshop/stockmanagement/userservice/service/IUserService.java
+++ src/main/java/com/microshop/stockmanagement/userservice/service/IUserService.java
@@ -1,4 +1,21 @@
 package com.microshop.stockmanagement.userservice.service;
 
+import com.microshop.stockmanagement.userservice.entity.User;
+import com.microshop.stockmanagement.userservice.enums.Language;
+import com.microshop.stockmanagement.userservice.request.UserCreateRequest;
+import com.microshop.stockmanagement.userservice.request.UserUpdateRequest;
+
+import java.util.List;
+
 public interface IUserService {
+
+    User createUser(Language language, UserCreateRequest userCreateRequest);
+
+    User getUser(Language language, Long userId);
+
+    List<User> getUsers(Language language);
+
+    User updateUser(Language language, Long userId, UserUpdateRequest userUpdateRequest);
+
+    User deleteUser(Language language, Long userId);
 }
