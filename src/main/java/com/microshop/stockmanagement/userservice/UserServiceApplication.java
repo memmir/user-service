d370eaec89b659190981d597fc9e65b12f21b460
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-15 21:53:11 +0300

    Added feign client dependency

--- src/main/java/com/microshop/stockmanagement/userservice/UserServiceApplication.java
+++ src/main/java/com/microshop/stockmanagement/userservice/UserServiceApplication.java
@@ -3,7 +3,11 @@ package com.microshop.stockmanagement.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class UserServiceApplication {
 
